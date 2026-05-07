1e2c0e43333d978c783296ddacd8fd1504bfffc6
TzuChen257 <elisa257italy@gmail.com>
2026-05-07 22:44:11 +0800

    更新

--- library/src/main/java/service/ReviewDraftsService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.ReviewDrafts;
-
-public interface ReviewDraftsService {
-    ReviewDrafts load(String reader_id, int slot);
-    void save(ReviewDrafts d);
-    void clear(String reader_id, int slot);
-}
